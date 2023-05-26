9b6b7c65f36786e503b10ac9ce3875e039b6694d
Jayne Valverde <jaynevalverde@gmail.com>
2023-05-26 13:18:37 -0500

    Api edit working

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,7 +5,7 @@ var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#cityname');
 var fcContainerEl = document.querySelector('#forecast-container');
 var fcSearchTerm = document.querySelector('#forecast-search-term');
-var forecastContainerEl = document.querySelector("weather-card");
+var forecastContainerEl = document.querySelector("#weather-card");
 
 var tempEl = document.querySelector('#Temperature');
 var windEl = document.querySelector('#Wind');
@@ -79,7 +79,7 @@ var weekAPI = function (cityname) {
     var { lat } = cityname;
     var { lon } = cityname;
 
-    var weekApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`;
+    var weekApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`;
 
     fetch(weekApiUrl)
         .then(function (response) {
@@ -96,33 +96,26 @@ var weekAPI = function (cityname) {
     var displayweekData = function (forecastData, fcSearchTerm) {
         for (i = 4; i < forecastData.list.length; i=i+4) {
 
-            var temp1El = document.querySelector("temp-1");
-            
-            
-
-            var date = document.createElement("h3");
-            // var temperature = document.createElement("h4");
-            temp1El = document.createElement('h4'); 
-
+            // CREATE ELEMENTS FOR DATA TO LIVE -----------------
+            var date = document.createElement("h4");
+            var temperature = document.createElement("h5");
             var humid = document.createElement("h5");
-            var winSpeed = document.createElement("h6");  
+            var winSpeed = document.createElement("h5");  
             
-                date.textContent = forecastData.list[i].dt.txt;
-                temp1El.textContent = "Temperature: " + forecastData.list[i].main.temp ;
-                humid.textContent = forecastData.list[i].main.humidity + "%";
-                winSpeed.textContent = forecastData.list[i].wind.speed + "mph";
+            // GRAB DATA AND TURN INTO READABLE TEXT CONTENT FOR PAGE
+            date.textContent = "Date: " + forecastData.list[i].dt_txt;
+            temperature.textContent = "Temperature: " + forecastData.list[i].main.temp + "F";
+            humid.textContent = "Humidity: " + forecastData.list[i].main.humidity + "%";
+            winSpeed.textContent = "Wind Speed: " + forecastData.list[i].wind.speed + "mph";
 
-                // forecastContainerEl.append(date);
-                forecastContainerEl.append(temp1El);
-                forecastContainerEl.append(humid);
-                forecastContainerEl.append(winSpeed);  
-        };
+            // APPEND DATA TO HTML VARIABLES -----------------
+            forecastContainerEl.append(date);
+            forecastContainerEl.append(temperature);
+            forecastContainerEl.append(humid);
+            forecastContainerEl.append(winSpeed);  
+        }
     }             
-    // Display Five Day 
-    // var displayWeek = function (week, fcSearchTerm) {
 
-
-    // }
 
     // Display forecast
     var displayForecast = function (forecast, fcSearchTerm) {
