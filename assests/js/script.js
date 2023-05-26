ac6d0f8a7207858d5dc28e7f59dc5e68b9efaf70
Jayne Valverde <jaynevalverde@gmail.com>
2023-05-26 12:31:11 -0500

    api append edits

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,6 +5,7 @@ var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#cityname');
 var fcContainerEl = document.querySelector('#forecast-container');
 var fcSearchTerm = document.querySelector('#forecast-search-term');
+var forecastContainerEl = document.querySelector("weather-card");
 
 var tempEl = document.querySelector('#Temperature');
 var windEl = document.querySelector('#Wind');
@@ -94,37 +95,29 @@ var weekAPI = function (cityname) {
 
     var displayweekData = function (forecastData, fcSearchTerm) {
         for (i = 4; i < forecastData.list.length; i=i+4) {
-            
-            var forecastContainer = document.getElementById("weather-card")
-            
-            // var date = document.querySelector("date-" + (i + 1));
-            // var temp = document.getElementById("Temp-" + (i + 1));
-            // var wind = document.getElementById("Wind-" + (i + 1));
-            // var humidity = document.getElementById("Humidity-" + (i + 1));
-            // var Week = [i];
 
-            // date.textContent = new Date(weatherData.dt * 1000).toLocaleDateString();
-            // temp.textContent ="Temperature: " + temperatureConverter(weatherData.main.temp) + "°F"
-            // wind.textContent = "Wind: " + '${wind.speed}'
-            // humidity.textContent = "Humidity: " + list.main.humidity;
-            // document.getElementById("weather-card").remove.Atrribute("style");
+            var temp1El = document.querySelector("temp-1");
+            
+            
 
             var date = document.createElement("h3");
-            var temperature = document.createElement("h4");
+            // var temperature = document.createElement("h4");
+            temp1El = document.createElement('h4'); 
+
             var humid = document.createElement("h5");
             var winSpeed = document.createElement("h6");  
             
-            date.textContent = forecastData.list[i].dt.txt;
-            temperature.textContent = forecastData.list[i].main.temp ;
-            humid.textContent = forecastData.list[i].main.humidity + "%";
-            winSpeed.textContent = forecastData.list[i].wind.speed + "mph";
+                date.textContent = forecastData.list[i].dt.txt;
+                temp1El.textContent = "Temperature: " + forecastData.list[i].main.temp ;
+                humid.textContent = forecastData.list[i].main.humidity + "%";
+                winSpeed.textContent = forecastData.list[i].wind.speed + "mph";
 
-            forecastContainer.append(date);
-            forecastContainer.append(temperature);
-            forecastContainer.append(humid);
-            forecastContainer.append(winSpeed);
-        }
-    }               
+                // forecastContainerEl.append(date);
+                forecastContainerEl.append(temp1El);
+                forecastContainerEl.append(humid);
+                forecastContainerEl.append(winSpeed);  
+        };
+    }             
     // Display Five Day 
     // var displayWeek = function (week, fcSearchTerm) {
 
@@ -135,11 +128,14 @@ var weekAPI = function (cityname) {
     var displayForecast = function (forecast, fcSearchTerm) {
         tempEl = document.createElement('p');
         windEl = document.createElement('p');
-        tempEl.textContent = "Temperature: " + forecast.main.temp, + " F";
-        windEl.textContent = "Wind Speed: " + forecast.wind.speed, + " Mph";
+        humidEl = document.createElement('p');
+        tempEl.textContent = "Temperature: " + forecast.main.temp + " F";
+        windEl.textContent = "Wind Speed: " + forecast.wind.speed + " Mph";
+        humidEl.textContent = "Humidity: " + forecast.main.humidity + "%";
         // console.log(forecast);
-        fcContainerEl.append(tempEl);
+        fcContainerEl.append(tempEl);          
         fcContainerEl.append(windEl);
+        fcContainerEl.append(humidEl);
 
     }
 
